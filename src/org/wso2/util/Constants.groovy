a60c6eceb2f9a95b7c561c264d5e12236013f67e
rasanjaya <rasanjaya@wso2.com>
2019-03-05 11:02:43 +0530

    Enable the DEPLOY_STAGING block

--- src/org/wso2/util/Constants.groovy
+++ src/org/wso2/util/Constants.groovy
@@ -36,7 +36,7 @@ class Constants {
         static final String SETUP_ENV = "Setup Environment"
         static final String BUILD_PACK = "Build the pack"
         static final String BUILD_IMAGE = "Build the immutable image"
-//        static final String DEPLOY_STAGING = "Deploy to Staging"
+        static final String DEPLOY_STAGING = "Deploy to Staging"
 //        static final String RUNNING_TESTS = "Running Tests"
 //        static final String DEPLOY_PRODUCTION = "Deploy to Production"
     }
